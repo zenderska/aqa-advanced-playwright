15cc84f0fc760fa4a408d4e568af5bab6cf96a0c
Viktor H <zenderska@gmail.com>
2026-03-26 19:55:13 +0200

    Revert "HM23_install_playwright_new_simple_test1"
    This reverts commit f6c58a6b0f0a8844fa2eb570ec907e750bcc6608.

--- tests/task-22.spec.ts
+++ tests/task-22.spec.ts
@@ -2,12 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('/');
+
   await expect(page).toHaveTitle('Hillel Qauto');
-});
-
-test('should display Garage button on homepage', async ({ page }) => {
-  await page.goto('/');
-
-  const garageButton = page.locator('text=Garage');
-  await expect(garageButton).toBeVisible();
 });
