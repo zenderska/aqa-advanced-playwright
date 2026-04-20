dc7d8494782fbbe6e650933577913cee5c551681
Viktor H <zenderska@gmail.com>
2026-04-20 18:36:32 +0300

    HM28_add_garagestate_to_fixture

--- fixtures/userGarage.fixture.js
+++ fixtures/userGarage.fixture.js
@@ -2,10 +2,18 @@ import { test as base } from '@playwright/test';
 import { GaragePage } from '../pages/GaragePage';
 
 export const test = base.extend({
-  userGaragePage: async ({ page }, use) => {
-  const garagePage = new GaragePage(page);
-  await use(garagePage);
-},
+  userGaragePage: async ({ browser }, use) => {
+    const context = await browser.newContext({
+      storageState: 'storageState.json',
+    });
+
+    const page = await context.newPage();
+    const garagePage = new GaragePage(page);
+
+    await use(garagePage);
+
+    await context.close();
+  },
 });
 
-export const expect = test.expect;
+export { expect } from '@playwright/test';
